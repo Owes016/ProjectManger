cacacf36e14e4ea8206e914f290409c26ffe26eb
Owes016 <owesknight@gmail.com>
2025-04-13 18:28:50 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: 'https://zevxyvzpkatnumcnmxzn.supabase.co',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpldnh5dnpwa2F0bnVtY25teHpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0NDc4NjEsImV4cCI6MjA2MDAyMzg2MX0.G12lxYbAFejG1cRrVuaHufKa5YOs59ojvvyAr9zn8N0',
   },
+  output: 'export',
+  basePath: '/ProjectManger',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
